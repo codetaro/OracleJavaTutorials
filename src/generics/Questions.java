8652ee3515fe499bec46661cd1b2d408330f45a3
iquesp <dennis.yuan@terrapages.com>
2018-12-07 18:21:53 +1100

    https://docs.oracle.com/javase/tutorial/java/generics/restrictions.html

---
+++ src/generics/Questions.java
@@ -0,0 +1,179 @@
+package generics;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+public class Questions {
+
+    public static void main(String[] args) {
+    }
+}
+
+// #1. Write a generic method to count the number of elements in a collection that have a specific property.
+class Answer1 {
+    public <E extends Number> int countPrimes(List<E> list) {
+        int cnt = 0;
+        for (E e : list) {
+            if (e.getClass().getName().equals("Prime")) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
+
+// #2. Will the following class compile?
+//* No. > operator applies only to primitive numeric types.
+
+// #3. Write a generic method to exchange the positions of two different elements in an array.
+class Answer2 {
+    public <T> void swap(T[] array, int i, int j) {
+        T tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
+    }
+}
+
+// #4. If the compiler erases all type parameters at compile time, why should you use generics?
+//* Java compiler enforces tighter type checks on generic code at compile time.
+//* Generics support programming types as parameters.
+//* Generics enable you to implement generic algorithms.
+
+// #5. What is the following class converted to after type erasure?
+class Pair3 {
+
+    public Pair3(Object key, Object value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public Object getKey() {
+        return key;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setKey(Object key) {
+        this.key = key;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+
+    private Object key;
+    private Object value;
+}
+
+// #6. What is the following method converted to after type erasure?
+class Answer6 {
+    public static int findFirstGreaterThan(Comparable[] at, Comparable elem) {
+        return -1;
+    }
+}
+
+// #7. Will the following method compile? If not, why?
+// Yes
+
+// #8. Write a generic method to find the maximal element in the range [begin, end) of a list.
+class Answer8 {
+    public <E extends java.lang.Comparable<E>> E maxInRange(List<E> list, int begin, int end) {
+        List<E> range = list.subList(begin, end);
+        E max = range.get(0);
+        for (E e : range) {
+            if (e.compareTo(max) > 0) {
+                max = e;
+            }
+        }
+        return max;
+    }
+}
+
+final class Solution8 {
+    public static <T extends Object & Comparable<? super T>>
+    T max(List<? extends T> list, int begin, int end) {
+        T maxElem = list.get(begin);
+        for (++begin; begin < end; ++begin)
+            if (maxElem.compareTo(list.get(begin)) < 0)
+                maxElem = list.get(begin);
+        return maxElem;
+    }
+}
+
+// #9. Will the following class compile? If not, why?
+// No, static field cannot be generic types
+
+// #10. Will the following code compile? If not, why?
+// No, Node<Circle> is not a subtype of Node<Shape>
+
+// #11. Will the following code compile? If not, why?
+// Yes
+
+// #12. How do you invoke the following method to find the first integer in a list that is relatively prime to a list of specified integers?
+interface UnaryPredicate<T> {
+    public boolean test(T obj);
+}
+
+class OddPredicate implements UnaryPredicate<Integer> {
+    @Override
+    public boolean test(Integer i) {
+        return i % 2 != 0;
+    }
+}
+
+final class Algorithm12 {
+
+    public static <T>
+    int findFirst(List<T> list, int begin, int end, UnaryPredicate<T> p) {
+
+        for (; begin < end; ++begin)
+            if (p.test(list.get(begin)))
+                return begin;
+        return -1;
+    }
+
+    // x > 0 and y > 0
+    public static int gcd(int x, int y) {
+        for (int r; (r = x % y) != 0; x = y, y = r) {
+        }
+        return y;
+    }
+}
+
+class RelativePrimePredicate implements UnaryPredicate<Integer> {
+    public RelativePrimePredicate(Collection<Integer> c) {
+        this.c = c;
+    }
+
+    @Override
+    public boolean test(Integer x) {
+        for (Integer i : c)
+            if (Algorithm12.gcd(x, i) != 1)
+                return false;
+        return c.size() > 0;
+    }
+
+    private Collection<Integer> c;
+}
+
+class Solution12 {
+
+    public static void main(String[] args) {
+
+        List<Integer> li = Arrays.asList(3, 4, 6, 8, 11, 15, 28, 32);
+        Collection<Integer> c = Arrays.asList(7, 18, 19, 25);
+        UnaryPredicate<Integer> p = new RelativePrimePredicate(c);
+
+        int i = Algorithm12.findFirst(li, 0, li.size(), p);
+
+        if (i != -1) {
+            System.out.print(li.get(i) + " is relatively prime to ");
+            for (Integer k : c)
+                System.out.print(k + " ");
+            System.out.println();
+        }
+    }
+}
--- src/generics/Restrictions.java
+++ src/generics/Restrictions.java
@@ -9,88 +9,76 @@ public class Restrictions {
 
     public static void main(String[] args) throws Exception {
 
-        /* Cannot instantiate generic types with primitive types */
-        // Pair<int, char> p = new Pair<>(8,'a');  // compile-time error
-        MyPair<Integer, Character> p = new MyPair<>(8, 'a');
+        // #1. Cannot Instantiate Generic Types with Primitive Types
+        Pair2<Integer, Character> p = new Pair2<>(8, 'a');
 
-        /* Cannot create instances of type parameters */
-        List<String> ls = new ArrayList<>();
-        append(ls, String.class);
-
-        /* Cannot declare static fields whose types are type parameters */
-        // MobileDevice<Smartphone> phone = new MobileDevice<>();
-        // MobileDevice<Pager> pager = new MobileDevice<>();
-        // MobileDevice<TabletPC> pc = new MobileDevice<>();
-
-        /* Cannot use casts or instanceof with parameterized types */
-        List<Integer> li = new ArrayList<>();
-        // List<Number> ln = (List<Number>) li;  // compile-time error
-        List<String> l1 = new ArrayList<>();
-        ArrayList<String> l2 = (ArrayList<String>) l1;
-
-        /* Cannot create arrays of parameterized types */
-        // List<Integer>[] arrayOfLists = new List<Integer>[2];  // compile-time error
-
-        /* Cannot create, catch, or throw objects of parameterized types */
-
+        // #5. Cannot Create Arrays of Parameterized Types
+        /*Object[] stringLists = new List<String>[2];
+        stringLists[0] = new ArrayList<String>();
+        stringLists[1] = new ArrayList<Integer>();*/
     }
 
+    // #2. Cannot Create Instances of Type Parameters
+    /*public static <E> void append(List<E> list) {
+        E elem = new E();
+        list.add(elem);
+    }*/
     public static <E> void append(List<E> list, Class<E> cls) throws Exception {
-        // E elem = new E();  // compile-time error
         E elem = cls.newInstance();
         list.add(elem);
     }
 
-    public static void rtti(List<?> list) {
-        // if (list instanceof ArrayList<Integer>) {  // compile-time error
-        if (list instanceof ArrayList<?>) {  // compile-time error
+    // #4. Cannot Use Casts or instanceof With Parameterized Types
+    public static <E> void rtti(List<E> list) {
+//        if (list instanceof ArrayList<Integer>) {}
+    }
 
+    public static void rtti2(List<?> list) {
+        if (list instanceof ArrayList<?>) {
         }
     }
 
-    public static <T extends Exception, J> void execute(List<J> jobs) {
+    // #6. Cannot Create, Catch, or Throw Objects of Parameterized Types
+    /*public static <T extends Exception, J> void execute(List<J> jobs) {
         try {
             for (J job : jobs) {
                 // ...
             }
-            // } catch (T e) {  // compile-time error
-        } catch (Exception e) {  // compile-time error
+        } catch (T e) {  // compile-time error
             // ...
         }
-    }
+    }*/
 }
 
-class MyPair<K, V> {
+class Pair2<K, V> {
 
     private K key;
     private V value;
 
-    public MyPair(K key, V value) {
+    public Pair2(K key, V value) {
         this.key = key;
         this.value = value;
     }
 }
 
+// #3. Cannot Declare Static Fields Whose Types are Type Parameters
 class MobileDevice<T> {
-
-    // private static T os;  // compile-time error
+//    private static T os;
 }
 
-// extends Throwable indirectly
-// class MathException<T> extends Exception {}
-
-// extends Throwable directly
-// class QueueFullException<T> extends Throwable {}
-
+// #6. Cannot Create, Catch, or Throw Objects of Parameterized Types
+//class MathException<T> extends Exception {}       // extends Throwable indirectly
+//class QueueFullException<T> extends Throwable {}  // extends Throwable directly
 class Parser<T extends Exception> {
 
     public void parse(File file) throws T {
-        // ...
     }
 }
 
+// #7. Cannot Overload a Method Where the Formal Parameter Types of Each Overload Erase to the Same Raw Type
 class Example {
-    public void print(Set<String> strSet) {
+
+    //    public void print(Set<String> strSet) {}
+    public void print(Set<Integer> intSet) {
     }
-//  public void print(Set<Integer> intSet) {}
 }
